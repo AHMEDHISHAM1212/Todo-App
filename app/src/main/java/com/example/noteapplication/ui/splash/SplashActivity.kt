07396210ea08f23b9480efc44b71341edc6dbbbd
AHMEDHISHAM1212 <a.hisham.desoky@gmail.com>
2024-04-21 01:08:18 -0700

    Home Views

---
+++ app/src/main/java/com/example/noteapplication/ui/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.noteapplication.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.noteapplication.R
+import com.example.noteapplication.ui.home.HomeActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startHomeActivity()
+    }
+
+    private fun startHomeActivity() {
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                startActivity(Intent(this, HomeActivity::class.java))
+            }
+                ,2000)
+    }
+}
